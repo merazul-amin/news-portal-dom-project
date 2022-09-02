0bc20483561e844bf21c41541c34798445ae0145
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-09-02 22:02:28 +0600

    load news by id done

--- js/app.js
+++ js/app.js
@@ -19,17 +19,42 @@ const loadCategory = (categorys) => {
     // console.log(categoriesArray)
 
     categoriesArray.forEach(category => {
-        console.log(category)
+        // console.log(category)
         const categorisContainer = document.getElementById('categories-container')
         const h5 = document.createElement('h5')
 
         h5.innerHTML = `
-        <h5 onclick="showNewsOfThisCatagory(${parseInt(category.category_id)})"><a class="text-decoration-none" href="">${category.category_name}</a></h5>
+        <h5 onclick="loadNewsData(${category.category_id})"  style="cursor: pointer;">${category.category_name}</h5>
         `
         categorisContainer.appendChild(h5)
     });
 
 }
 
-//Now a function for show news by category clicking
+//Now a function for load and show news by category clicking
+const loadNewsData = (id) => {
+    // console.log(typeof id)
+    // console.log(id)
+    try {
+        fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+            .then(res => res.json())
+            .then(data => showNews(data))
+    }
+    catch {
+
+    }
+
+}
+
+
+const showNews = (news) => {
+    console.log(news)
+    //set the number of category news
+    const categoriesNewsNumber = document.getElementById('category-items')
+    categoriesNewsNumber.innerHTML = `
+    <p></p>
+    `
+}
+
+
 
